ac9f16714b0770762bd0692d0f2f39f474dc7264
Caider08 <caidbuch@ksu.edu>
2018-10-03 23:55:50 -0600

    start of api solution try

--- QuranMEM/QuranMEM/VerseSelectionPage.xaml.cs
+++ QuranMEM/QuranMEM/VerseSelectionPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,25 @@ namespace QuranMEM
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void QuranCloudTest_Clicked(object sender, EventArgs e)
+        { 
+
+            
+
+       string url = "http://api.alquran.cloud/ayah/262/en.asad";
+
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString(url);
+
+               
+
+                response.ToString();
+            }
+
+
+
+        }
+    }
 }
